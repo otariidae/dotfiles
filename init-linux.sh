8b61eba77e32c0771b161bd5f912b55e84b28430
otariidae <otariidae@users.noreply.github.com>
2020-11-11 01:07:00 +0900

    remove init-linux.sh

--- init-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/otariidae/dotfiles.git
-ln -s dotfiles/.vimrc ~/.vimrc
-git clone https://github.com/k-takata/minpac.git \
-    ~/.vim/pack/minpac/opt/minpac
